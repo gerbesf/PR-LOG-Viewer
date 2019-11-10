dbb6073bce8a0f86a41fd90c1851db785eb0bd1f
Gerbes Matos <gerbes.matos@gmail.com>
2019-11-10 15:26:16 -0300

    remove example code

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,2 @@
 
 var Application = angular.module('App', ['ngSanitize']);
-
-
-window.checkTime = function ( expiration ) {
-
-    //console.log(new Date().toISOString());
-    //console.log(new Date(expiration).toISOString( { timeZone:  window.timeZone}));
-}
