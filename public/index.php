716bb9cace8019dce50d429ce93d92d3931b4c47
Zorak <gerbes.matos@gmail.com>
2020-07-31 13:42:42 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -19,7 +19,9 @@ if($Session->isLogged()==false && $GLOBALS['config']['require_login']==true){
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
@@ -112,7 +114,7 @@ if($Session->isLogged()==false && $GLOBALS['config']['require_login']==true){
                     <button class="btn btn-link  float-right" ng-click="setServer(server);downloadLog(server);toogleServer(server.id)">
                         <div ng-show="server.loading">Loading</div>
                         <div ng-hide="server.loading">
-                            <small><span class="fa fa-time"> </span> Updated on: <span ng-if="!server.timestamp">loading...</span> {{ server.timestamp }}</small>
+                            <small><span class="fa fa-refresh"> </span> Updated on: <span ng-if="!server.timestamp">loading...</span> {{ server.timestamp }}</small>
                         </div>
                     </button>
                 </li>
